920f547d79a50dc73bec83f369aa92e350050b1a
jgonzal <pepegonzal@yahoo.es>
2016-01-21 00:28:49 +0100

    Empiezo con colopicker

---
+++ app/src/main/java/com/example/cdp/mispartidas/actividades/Duelo.java
@@ -0,0 +1,16 @@
+package com.example.cdp.mispartidas.actividades;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cdp.mispartidas.R;
+
+public class Duelo extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duelo);
+    }
+}
