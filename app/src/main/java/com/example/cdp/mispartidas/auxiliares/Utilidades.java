d1bb83f492b2f158feae3bf9eea49eb02b64b45e
pezpan <pepegonzal@yahoo.es>
2016-01-26 12:23:54 +0100

    Update Utilidades.java

--- app/src/main/java/com/example/cdp/mispartidas/auxiliares/Utilidades.java
+++ app/src/main/java/com/example/cdp/mispartidas/auxiliares/Utilidades.java
@@ -29,4 +29,25 @@ public class Utilidades {
         String hexColor = String.format("#%06X", (0xFFFFFF & intcolor));
         return hexColor;
     }
+    
+    public static void buttonEffect(View button){
+        button.setOnTouchListener(new OnTouchListener() {
+    
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        v.getBackground().setColorFilter(0xe0f47521,PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+}
 }
