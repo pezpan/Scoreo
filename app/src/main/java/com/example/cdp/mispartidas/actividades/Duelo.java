d505bb54b22c31fd5f07fee2f545ee299cf91665
pezpan <pepegonzal@yahoo.es>
2016-02-01 15:14:46 +0100

    Update Duelo.java

--- app/src/main/java/com/example/cdp/mispartidas/actividades/Duelo.java
+++ app/src/main/java/com/example/cdp/mispartidas/actividades/Duelo.java
@@ -12,6 +12,28 @@ public class Duelo extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duelo);
+        
+        Log.i("MILOG", "Obtenemos el backup");
+        backup = Backup.getMiBackup(getApplicationContext());
+
+        // Obtenemos el numero de jugadores
+        Bundle bundle = getIntent().getExtras();
+        identificador = bundle.getString("idpartida");
+
+        Log.i("MILOG", "El identificador de la partida es " + identificador);
+
+        // Habilitamos la fecha volver a la activity principal
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        // Buscamos la partida
+        indice = backup.getPartida(identificador);
+        if (indice >= 0) {
+            
+        } else {
+            Toast.makeText(this, "No se ha encontrado la partida " + identificador, Toast.LENGTH_SHORT).show();
+        }
+        
+        
     }
     
     
