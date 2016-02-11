fece3c057962a5621248a779b2c7d2c242f5a448
pezpan <pepegonzal@yahoo.es>
2016-02-11 17:43:58 +0100

    Update Settings.java

--- app/src/main/java/com/example/cdp/mispartidas/preferencias/Settings.java
+++ app/src/main/java/com/example/cdp/mispartidas/preferencias/Settings.java
@@ -3,5 +3,11 @@ package com.example.cdp.mispartidas.preferencias;
 /**
  * Created by CDP on 11/02/2016.
  */
-public class Settings {
+public class Settings extends PreferenceActivity{
+  
+  @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferencias);
+    }
 }
